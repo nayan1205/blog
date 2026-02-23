73c19a028ded4856e7d5a9e22c0377389a8330df
Nayan Gupta <nyngtm@gmail.com>
2026-02-24 00:50:43 +0530

    Initial Commit

---
+++ BLOG/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save()  # Call the original save method to save the profile
+
+        # Resize the image if it's larger than 300x300 pixels
+        from PIL import Image
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+# Create your models here.
