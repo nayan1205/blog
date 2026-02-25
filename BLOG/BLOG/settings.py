1b2b5888d45608f79ec1c6f8392bb58ea38acf2e
Nayan Gupta <166999837+nayan1205@users.noreply.github.com>
2026-02-25 23:49:32 +0530

    Switch database configuration to use dj_database_url

--- BLOG/BLOG/settings.py
+++ BLOG/BLOG/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -81,11 +82,16 @@ WSGI_APPLICATION = 'BLOG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
