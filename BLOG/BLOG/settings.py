9275ec17d00f1b8d7e866dcefc26b445599c6ace
Nayan Gupta <nyngtm@gmail.com>
2026-02-28 23:53:32 +0530

    Fix PostgreSQL driver

--- BLOG/BLOG/settings.py
+++ BLOG/BLOG/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'BLOG.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
